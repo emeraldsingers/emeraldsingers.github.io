0084db8389cb31b4ffc3932242644fd23e0a34cf
1 <zonal9999999@gmail.com>
2025-02-19 23:23:19 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base: "./",
   plugins: [
     react(),
     mode === 'development'
