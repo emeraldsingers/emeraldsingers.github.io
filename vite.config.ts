5431dcb8f1a7839cfd1c08b0ff1a0ae226269adf
emeraldsingers <109750346+emeraldsingers@users.noreply.github.com>
2025-02-19 23:29:09 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
 export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "/" : "/",
-  plugins: [
-    react(),
-    mode === "development"
-  ].filter(Boolean),
+  base: "/", 
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist",
   },
 }));
