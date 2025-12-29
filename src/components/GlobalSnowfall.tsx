c8b0147b65da336822afad58698158a8161ad87d
asoqwer <zonal9999999@gmail.com>
2025-12-29 16:00:02 +0200

    snowfall

--- src/components/GlobalSnowfall.tsx
+++ src/components/GlobalSnowfall.tsx
@@ -6,7 +6,7 @@ const GlobalSnowfall = () => {
 
   return (
     <Snowfall
-      color={theme === "dark" ? "rgba(54, 128, 67, 0.8)" : "rgba(0, 255, 157, 1.0)"}
+      color={theme === "dark" ? "rgba(255, 255, 255, 0.3)" : "rgba(0, 255, 157, 1.0)"}
       snowflakeCount={250}
       style={{ position: "fixed", width: "100vw", height: "100vh", zIndex: 0 }}
     />
