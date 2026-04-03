b8c0a1261c2f05ad4c8f9fcf8ce7f4e012838b91
asoqwer <zonal9999999@gmail.com>
2026-04-03 20:53:54 +0300

    change link

--- src/pages/AboutUtauV.tsx
+++ src/pages/AboutUtauV.tsx
@@ -234,7 +234,7 @@ const AboutUtauV = () => {
                     >
                         <Button size="lg" asChild>
                             <a 
-                                href="https://github.com/emeraldsingers/UtauV/releases/download/v1.1.0.0/UtauV.1.1.0.0.1.Installer.exe" 
+                                href="https://github.com/emeraldsingers/UtauV/releases/latest" 
                                 target="_blank" 
                                 rel="noopener noreferrer"
                                 className="flex items-center"
